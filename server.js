125312f64154f7ee2d74f9d1fe98aba4145c1b1d
Mike Bild <mike@mikebild.com>
2015-06-27 05:52:40 +0200

    Fix mapping

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var proxy = require('http-proxy').createProxyServer();
 var http = require('http');
 var serviceSDK = require('lc-sdk-node.js');
@@ -6,7 +8,8 @@ var DISCOVERY_SERVICE_URLS = (process.env.DISCOVERY_SERVICE_URLS || '').split(/\
 var serviceClient = serviceSDK({ discoveryServers: DISCOVERY_SERVICE_URLS, timeout: 2000 });
 var availableUrls = [];
 
-resolve();
+retryResolve();
+
 var server = http.createServer();
 server.addListener('request', handler);
 server.listen(process.env.SERVICE_PORT, function(){
@@ -14,11 +17,26 @@ server.listen(process.env.SERVICE_PORT, function(){
 });
 
 function handler(req, res){
+  var endFunc = res.end;
+  res.end = function(){
+    console.log('ERROR: no map found ' + req.headers.host)
+    endFunc();
+  };
+
+  if(!req.headers.host) return res.end();
   var requestedHost = req.headers.host.split(':')[0];  
+
+  if(!requestedHost) return res.end();  
   var mapUrl = process.env[requestedHost.toUpperCase()];
 
-  if(availableUrls.indexOf(mapUrl) !== -1) proxy.web(req, res, { target: 'http://' + mapUrl });
-  else res.end();
+  if(!mapUrl) return res.end();
+  if(availableUrls.indexOf(mapUrl) !== -1) {
+    console.log('Map ' + requestedHost + ' -> ' + mapUrl)
+    proxy.web(req, res, { target: 'http://' + mapUrl });
+  }
+  else {
+    res.end();
+  }
 }
 
 function resolve(){
@@ -33,10 +51,14 @@ function resolve(){
       return merged;
     })
     .then(function(result){
-      console.log(result);
-      availableUrls = result;      
+      availableUrls = result;
+      console.log('Discovery done');
     })
     .catch(function(error){
-      console.log(error);
+      console.log('ERROR: ' + error.message)
     });
+}
+
+function retryResolve(){
+  setInterval(resolve, 5000);
 }
